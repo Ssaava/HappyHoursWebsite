85234ab5250b7f1eb55ca0d356bb186e4db5442c
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-10-20 13:17:46 +0300

    changed songs folder

--- SONGS/music-list.js
+++ SONGS/music-list.js
@@ -10,59 +10,59 @@
 //paste it inside the array as more as you want music then you don't need to do any other thing
 
 let allMusic = [
-    {
-      name: "Abazade",
-      artist: "School Choir",
-      img: "Abazade",
-      src: "Abazade"     
-    },
-    {
-      name: "Congratulations",
-      artist: "School Choir",
-      img: "Congratulations",
-      src: "Congratulations"
-    },
-    {
-      name: "Gyetuvude",
-      artist: "School Choir",
-      img: "Gyetuvude",
-      src: "Gyetuvude"
-    },
-    {
-      name: "Jesus Hold My Hand",
-      artist: "School Choir",
-      img: "Jesus Hold My Hand",
-      src: "Jesus Hold My Hand"
-    },
-    {
-      name: "Noah",
-      artist: "school Choir",
-      img: "Noah",
-      src: "Noah"
-    },
-    {
-      name: "School Anthem",
-      artist: "School Choir",
-      img: "School Anthem",
-      src: "School Anthem"
-    },
-    {
-      name: "Tewali Akwenkana",
-      artist: "School Choir",
-      img: "Tewali Akwenkana",
-      src: "Tewali Akwenkana"
-    },
-     {
-      name: "Tulina Katonda",
-      artist: "School Choir",
-      img: "Tulina Katonda",
-      src: "Tulina Katonda"
-    },
-    // like this paste it and remember to give comma after ending of this bracket }
-    // {
-    //   name: "Here is the music name",
-    //   artist: "Here is the artist name",
-    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-    // }
-  ];
+  {
+    name: "Abazade",
+    artist: "School Choir",
+    img: "Abazade",
+    src: "Abazade",
+  },
+  {
+    name: "Congratulations",
+    artist: "School Choir",
+    img: "Congratulations",
+    src: "Congratulations",
+  },
+  {
+    name: "Gyetuvude",
+    artist: "School Choir",
+    img: "Gyetuvude",
+    src: "Gyetuvude",
+  },
+  {
+    name: "Jesus Hold My Hand",
+    artist: "School Choir",
+    img: "Jesus Hold My Hand",
+    src: "Jesus Hold My Hand",
+  },
+  {
+    name: "Noah",
+    artist: "school Choir",
+    img: "Noah",
+    src: "Noah",
+  },
+  {
+    name: "School Anthem",
+    artist: "School Choir",
+    img: "School Anthem",
+    src: "School Anthem",
+  },
+  {
+    name: "Tewali Akwenkana",
+    artist: "School Choir",
+    img: "Tewali Akwenkana",
+    src: "Tewali Akwenkana",
+  },
+  {
+    name: "Tulina Katonda",
+    artist: "School Choir",
+    img: "Tulina Katonda",
+    src: "Tulina Katonda",
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
